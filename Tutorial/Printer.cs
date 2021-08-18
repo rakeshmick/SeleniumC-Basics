e3cabdfb9c506e2c9378157a99ba94b3b62b71ae
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2021-08-18 20:27:04 +1000

    first commit

---
+++ Tutorial/Printer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial
+{
+    class Printer
+    {
+        public void printFunction(String value) {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ Tutorial/TestClass.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Tutorial
+{
+    class TestClass
+    {
+      
+
+        [Test]
+        public void seleniumFirstTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+               
+                driver.Url = "http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f";
+                IWebElement usernameField = driver.FindElement(By.Id("UserName"));
+                usernameField.SendKeys("hari");
+                driver.FindElement(By.Id("Password")).SendKeys("123123");
+                driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
+                IWebElement dashBoardMenuButton = driver.FindElement(By.ClassName("dropdown-toggle"));
+                Assert.IsTrue(dashBoardMenuButton.Displayed);
+
+                driver.FindElement(By.CssSelector(".dropdown-menu")).Click();
+
+                ReadOnlyCollection<IWebElement> menu = driver.FindElements(By.CssSelector("div.navbar.navbar-inverse.navbar-fixed-top > div > div > ul > li.dropdown.open > ul li"));
+            
+                foreach (IWebElement link in menu)
+                {
+                    string text = link.Text;
+                    if (Regex.IsMatch("Time & Materials", text))
+                    {
+                        link.Click();
+                    }
+                }
+
+                IWebElement createNewButton = driver.FindElement(By.CssSelector(".btn.btn-primary"));
+                Assert.IsTrue(createNewButton.Displayed);
+                driver.Close();
+                
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                driver.Close();
+                driver.Quit();
+            }
+
+
+
+        }
+
+    }
+}
